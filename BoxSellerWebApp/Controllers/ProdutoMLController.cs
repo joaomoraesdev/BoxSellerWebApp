35d6670ed5150d4a3b6639ce8857445f43edf560
joaomoraesdev <jmoraes2003@hotmail.com>
2025-07-11 10:42:48 -0300

    upd: renome pasta e exclusão pasta out

--- BoxSellerWebApp/Controllers/ProdutoMLController.cs
+++ BoxSellerWebApp/Controllers/ProdutoMLController.cs
@@ -22,7 +22,7 @@ namespace BoxSellerWebApp.Controllers
         public IActionResult MenuProduto()
         {
             ViewBag.NomeAplicacao = HttpContext.Session.GetString("NomeAplicacao");
-            return View("~/Views/MercadoLivre/MenuProduto.cshtml");
+            return View("~/Views/ML/MenuProduto.cshtml");
         }
 
         [HttpGet]
